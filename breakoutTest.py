977e59158032b0db32500a482ea80299e8c2f4bf
tangylyre <tangylyre@gmail.com>
2021-03-03 20:23:25 -0500

    breakout continued

--- breakoutTest.py
+++ breakoutTest.py
@@ -1,15 +1,18 @@
 from eogCore import initDAC, setVoltsNorm
-
+from time import sleep
 motor = initDAC()
 i = 0.1
 x = 0
 try:
     while True:
-        if x == 1 or x == 0:
-            i *= -1
+        if x >= 1:
+            i = -0.1
+        elif x <= 0:
+            i = 0.1
         setVoltsNorm(motor, x)
         x += i
         print(x)
+        sleep(1)
 except KeyboardInterrupt:
     setVoltsNorm(motor, 0)
     print("stopping..")
