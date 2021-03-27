ad4063c31cee66b55f4e21d63c6bd83c23b30fbd
tangylyre <tangylyre@gmail.com>
2021-03-27 17:42:45 -0400

    add

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from eogCore import *
 
 y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10]
 yf = fourTransMag(y)
-print(y)
+print(yf)
 
 y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 30, 31, 203, 31, 34]
 yf = fourTransMag(y)
-print(y)
+print(yf)
