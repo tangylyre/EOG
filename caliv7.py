5b54ef04174846fdcdf6dae64f444ffc7433562a
tangylyre <tangylyre@gmail.com>
2021-04-07 19:45:27 -0400

    reorg, bugfix

--- caliv7.py
+++ caliv7.py
@@ -4,6 +4,11 @@ import time
 
 
 def calibrationV7Four(t, Hz, eogChan):
+    query = input("please input 'y' if you want to display plots, enter if not.")
+    if query == 'y':
+        displayPlots = True
+    else:
+        displayPlots = False
     engine = initSpeechEngine()
     s = "Please look straight ahead for %d seconds. You will be signaled to stop." % t
     print(s)
@@ -20,34 +25,35 @@ def calibrationV7Four(t, Hz, eogChan):
     time.sleep(1)
     print("done.")
     weightedProfile, threshScore = makeFourierThresholds(Yneu, Ydis)
-    fig, plt, ax, line = initPlotFour(xfDis, yfNeu)
-    print("displaying fourier of neutral..")
-    speakString("displaying fourier of neutral..", engine)
-    updatePlt(plt, line, yfNeu, Hz)
-    input("press enter to continue.")
-    print("displaying fourier of distress..")
-    speakString("displaying fourier of distress..", engine)
-    updatePlt(plt, line, yfDis, Hz)
-    input("press enter to continue.")
-    print("displaying weightedProfile..")
-    speakString("displaying fourier of weighted profile..", engine)
-    plt.xlim([0, 50])
-    plt.ylim([0, 1])
-    ax.clear()
-    graph = plt.plot(xfDis, weightedProfile)[0]
-    input("press enter to continue.")
-    print("displaying raw voltage of neutral..")
-    speakString("displaying raw voltage of neutral..", engine)
-    ax.clear()
-    graph = plt.plot(Xneu, Yneu)[0]
-    plt.xlim([0, t])
-    plt.ylim([0, 3.5])
-    input("press enter to continue.")
-    ax.clear()
-    graph = plt.plot(Xneu, Ydis)[0]
-    print("displaying raw voltage of distress..")
-    speakString("displaying raw voltage of distress..", engine)
-    input("press enter to continue.")
+    if displayPlots:
+        fig, plt, ax, line = initPlotFour(xfDis, yfNeu)
+        print("displaying fourier of neutral..")
+        speakString("displaying fourier of neutral..", engine)
+        updatePlt(plt, line, yfNeu, Hz)
+        input("press enter to continue.")
+        print("displaying fourier of distress..")
+        speakString("displaying fourier of distress..", engine)
+        updatePlt(plt, line, yfDis, Hz)
+        input("press enter to continue.")
+        print("displaying weightedProfile..")
+        speakString("displaying fourier of weighted profile..", engine)
+        plt.xlim([0, 50])
+        plt.ylim([0, 1])
+        ax.clear()
+        graph = plt.plot(xfDis, weightedProfile)[0]
+        input("press enter to continue.")
+        print("displaying raw voltage of neutral..")
+        speakString("displaying raw voltage of neutral..", engine)
+        ax.clear()
+        graph = plt.plot(Xneu, Yneu)[0]
+        plt.xlim([0, t])
+        plt.ylim([0, 3.5])
+        input("press enter to continue.")
+        ax.clear()
+        graph = plt.plot(Xneu, Ydis)[0]
+        print("displaying raw voltage of distress..")
+        speakString("displaying raw voltage of distress..", engine)
+        input("press enter to continue.")
     filename = input("input filename, or none for default\n")
     if len(filename) < 1:
         filename = "calibration_profile_%dHz_%dseconds.cali" % (Hz, t)
