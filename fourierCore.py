2032062b2bd19d36b8398e791ebab4fd2ebeca73
tangylyre <tangylyre@gmail.com>
2021-04-12 16:18:59 -0400

    partitioning methods

--- fourierCore.py
+++ fourierCore.py
@@ -78,9 +78,8 @@ def subtractFourier(list1, list2):
 def makeWeightProfile(eqFour):
     # accepts a normalized equalized profile, assigns each frequency a weight from 0 - 1 in relation to this numpy
     # array's maxima.
-    weightedProfile = eqFour
+    weightedProfile = np.array(len(eqFour))
     maxVal = np.amax(weightedProfile)
-    print(eqFour)
     for i in range(len(eqFour)):
         current = eqFour[i]
         weightedProfile[i] = current / maxVal
