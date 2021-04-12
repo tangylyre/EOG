18140f8a1c5bb6b9549c4e50675c77d79d8f012f
tangylyre <tangylyre@gmail.com>
2021-04-12 15:58:14 -0400

    partitioning methods

--- fourierCore.py
+++ fourierCore.py
@@ -78,6 +78,7 @@ def makeWeightProfile(eqFour):
     # array's maxima.
     weightedProfile = eqFour
     maxVal = np.amax(eqFour)
+    print(eqFour)
     print(maxVal)
     for i in range(len(eqFour)):
         weightedProfile[i] = eqFour[i] / maxVal
