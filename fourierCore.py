c92e4275fc6d1647c14208bfd1c681ed15e9f76b
tangylyre <tangylyre@gmail.com>
2021-04-12 17:58:47 -0400

    partitioning methods

--- fourierCore.py
+++ fourierCore.py
@@ -42,12 +42,12 @@ def pullFourierProfile(t, Hz, eogChan, voiceEngine, speech):
     if speech:
         speakString("Finished.", voiceEngine)
     yf = fourTransMag(Y)
-    return [X, Y, xf[0:201], fourierFilter(yf)]
+    return [X, Y, xf[0:200], fourierFilter(yf)]
 
 
 def fourierFilter(yf):
     # acts as a bandpass filter from 1 - 20 Hz, setting values <1 to 0 and removing frequencies past 50Hz.
-    yf = yf[0:199]
+    yf = yf[0:200]
     for i in range(10):
         yf[i] = 0
     return yf
