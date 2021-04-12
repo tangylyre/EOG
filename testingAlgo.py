95a96cbf3f4ed5362775458d0bab51d58ebd3fcf
tangylyre <tangylyre@gmail.com>
2021-04-12 15:20:49 -0400

    partitioning methods

--- testingAlgo.py
+++ testingAlgo.py
@@ -6,7 +6,8 @@ hz = 500
 rf = 10
 X, Y, xf, yf = initVals(rf, hz)
 freq, neutral, distress = getFourierData("joshcali1.cali")
-fig, plt, ax, line = initPlotFour(freq, neutral)
+X, Y, xf, yf, fig, plt, ax, line = initPlot(rf, hz)
+updatePlt(plt, line, neutral, hz)
 input('press enter')
 updatePlt(plt, line, distress, hz)
 normalize = subtractFourier(distress, neutral)
