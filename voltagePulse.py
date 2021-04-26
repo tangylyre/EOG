03d85f0c35f3a9f1942053ec34baaef281ebd975
tangylyre <tangylyre@gmail.com>
2021-04-26 19:51:44 -0400

    breakout test

--- voltagePulse.py
+++ voltagePulse.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+GPIO.setup(1, GPIO.OUT)
 while True:
-    GPIO.output(11, True)
+    GPIO.output(1, True)
     time.sleep(10)
-    GPIO.output(11, False)
+    GPIO.output(1, False)
     time.sleep(10)
