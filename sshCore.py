5828a6e41ed462c57bca9b3c8849dec0a9e381fa
tangylyre <tangylyre@gmail.com>
2021-05-03 19:05:17 -0400

    breakout test

--- sshCore.py
+++ sshCore.py
@@ -5,11 +5,11 @@ from time import sleep
 def initSSH():
     client = SSHClient()
     client.load_system_host_keys()
-    #client.load_host_keys('~/.ssh/known_hosts')
+    # client.load_host_keys('~/.ssh/known_hosts')
     client.set_missing_host_key_policy(AutoAddPolicy())
     # client.look_for_keys(True)
     ip = '192.168.1.214'
-    client.connect(ip, username='pi', passphrase='doc')
+    client.connect(ip, username='pi', passphrase='doc', password='doc', pkey=None)
     sshCommand(client, 'cd EOG/SSHScripts')
     return client
 
