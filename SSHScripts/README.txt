70172e85593d5ec7b21ee58e2c69479b4962213c
tangylyre <tangylyre@gmail.com>
2021-05-03 18:50:27 -0400

    breakout test

---
+++ SSHScripts/README.txt
@@ -0,0 +1,3 @@
+This folder is for scripts I can call via SSH.
+Basically, function calls through SSH is a nightmare the way I wrote our SSH protocol.
+Each script serves the identical method, just easier called instead of passing arguments into the function.
--- sshCore.py
+++ sshCore.py
@@ -1,5 +1,5 @@
 from paramiko import SSHClient
-from eogCore import *
+from time import sleep
 
 
 def initSSH():
@@ -10,7 +10,7 @@ def initSSH():
     client.look_for_keys(True)
     ip = '192.168.1.214'
     client.connect(ip, username='pi', passphrase='doc')
-    sshCommand(client, 'cd EOG')
+    sshCommand(client, 'cd EOG/SSHScripts')
     return client
 
 
@@ -20,20 +20,29 @@ def sshCommand(client, string):
     return
 
 
-def motorInitSSH(client):
-    string = "motorInit()"
-    sshCommand(client, string)
-
-
 def motorControlSSH(client, setting):
-    string = "motorControl(" + setting + ')'
+    if setting == "Coarse":
+        string = 'python3 pulseCoarse.py'
+    elif setting == "Fine":
+        string = 'python3 pulseCoarse.py'
     sshCommand(client, string)
 
 
 def motorKillSSH(client):
-    string = "motorKill()"
+    string = "python3 pulseKill.py"
     sshCommand(client, string)
+
 
 def sshTest():
     SSH = initSSH()
-    
+    motorControlSSH(SSH, "Fine")
+    sleep(5)
+    motorKillSSH(SSH)
+    sleep(5)
+    motorControlSSH(SSH, "Coarse")
+    sleep(5)
+    motorKillSSH(SSH)
+
+
+if __name__ == '__main__':
+    sshTest()
